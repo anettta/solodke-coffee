40b1a8278a9497118f6737ea2e9816d2ffb7ece0
Anya <annagapyuk@gmail.com>
2021-03-09 16:41:00 -0500

    changed variable name

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,9 +22,8 @@ module.exports = {
     {
      resolve: `gatsby-source-contentful`,
      options: {
-       spaceId: process.env.CONTENTFUL_ID,
+       spaceId: process.env.CONTENTFUL_SPACE_ID,
        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-
      },
     },
     `gatsby-transformer-sharp`,
